4ef456408e2c51a9f38ad183111d14e52f41f478
0404233 <andrey0404233@gmail.com>
2025-01-13 22:43:49 +0300

    feat: done getNearestBigger task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -536,10 +536,53 @@ function shuffleChar(str, iterations) {
  * @param {number} number The source number
  * @returns {number} The nearest larger number, or original number if none exists.
  */
-function getNearestBigger(/* number */) {
-  throw new Error('Not implemented');
-}
+function getNearestBigger(number) {
+  const numbersVariants = [];
+  let temp = number;
 
+  while (temp > 0) {
+    numbersVariants.unshift(temp % 10);
+    temp = Math.floor(temp / 10);
+  }
+
+  const n = numbersVariants.length;
+
+  let i = n - 2;
+
+  while (i >= 0 && numbersVariants[i] >= numbersVariants[i + 1]) {
+    i -= 1;
+  }
+
+  if (i < 0) return number;
+
+  let j = n - 1;
+  while (numbersVariants[j] <= numbersVariants[i]) {
+    j -= 1;
+  }
+  [numbersVariants[i], numbersVariants[j]] = [
+    numbersVariants[j],
+    numbersVariants[i],
+  ];
+
+  let left = i + 1;
+  let right = n - 1;
+  while (left < right) {
+    [numbersVariants[left], numbersVariants[right]] = [
+      numbersVariants[right],
+      numbersVariants[left],
+    ];
+    left += 1;
+    right -= 1;
+  }
+
+  let result = 0;
+  for (let v = 0; v < numbersVariants.length; v += 1) {
+    result = result * 10 + numbersVariants[v];
+  }
+
+  return result;
+}
+getNearestBigger();
 module.exports = {
   isPositive,
   getMaxNumber,
